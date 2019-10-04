573f62e10ec2d5f1a98c612425cd84495d42694a
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2019-10-04 21:48:57 +0530

    code modified as function

--- ping_test.py
+++ ping_test.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
 from subprocess import run
+file_name = input("Enter the file name which has the IP address listed\n")
+def ping_check(file):
 
+	with open(file) as f:
+		for line in f:
+			addr = line.strip()
+			# print(type(addr))
+			a = run(['ping', '-c1', addr], capture_output=True)
+			if a.returncode == 0:
+				print(f"{addr} is UP")
+			else:
+				print(f"{addr} is DOWN")	
 
-with open('jrs_ip') as f:
-	for line in f:
-		addr = line.strip()
-		# print(type(addr))
-		a = run(['ping', '-c1', addr], capture_output=True)
-		if a.returncode == 0:
-			print(f"{addr} is UP")
-		else:
-			print(f"{addr} is DOWN")	
-
+ping_check(file_name)
